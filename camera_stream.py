e7e539a95ed7c979e8ce4adfa4447351b3308c9e
FengXingZhe666 <fxzzyk@gmail.com>
2025-05-17 15:56:01 +0800

    update20250517

---
+++ camera_stream.py
@@ -0,0 +1,28 @@
+import cv2
+from detect_zzk import run as yolov5_detect
+
+# 打开摄像头
+cap = cv2.VideoCapture(0)
+if not cap.isOpened():
+    print("Error: Cannot open camera")
+    exit()
+
+while True:
+    # 读取一帧
+    ret, frame = cap.read()
+    if not ret:
+        print("Error: Cannot receive frame (stream end?). Exiting ...")
+        break
+
+    # 执行yolov5检测
+
+
+    # 显示帧
+    cv2.imshow('Camera Feed', frame)
+    # 按下'q'键退出
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+# 释放资源并关闭窗口
+cap.release()
+cv2.destroyAllWindows()
